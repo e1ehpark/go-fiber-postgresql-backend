806c705262730d538e910b48bc5c985f510d0ef3
e1ehpark <ehpark@enterprise1.co.kr>
2024-02-14 08:58:20 +0900

    ctl 수정

--- controllers/note.controller.go
+++ controllers/note.controller.go
@@ -1,18 +1,17 @@
 package controllers
 
 import (
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/e1ehpark/go-fiber-postgresql-backend/initializers"
 	"github.com/e1ehpark/go-fiber-postgresql-backend/models"
+	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
 func CreateNoteHandler(c *fiber.Ctx) error {
-	varr payload *models.CreageNoteSchema
+	var payload *models.CreageNoteSchema
 
 	if err := c.BodyParser(&payload); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
@@ -35,7 +34,7 @@ func CreateNoteHandler(c *fiber.Ctx) error {
 
 	result := initializers.DB.Create(&newNote)
 
-	if result.Error != nil && strings.Contains(result.Error.Error(),"duplicate key value violates unique") {
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
 		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"status": "fail", "message": "Title already exist, please use another title"})
 	} else if result.Error != nil {
 		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "error", "message": result.Error.Error()})
